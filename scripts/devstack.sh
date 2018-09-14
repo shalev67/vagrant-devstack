00fa5585886e1f4ca5ba32aa92a4ac0ca840c953
Yuval Shalev <shalev67@gmail.com>
2018-09-14 16:13:51 +0300

    Added ceph plugin

--- scripts/devstack.sh
+++ scripts/devstack.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install -y git
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+
+sudo apt update && sudo apt install -y git uuid-runtime iptables iptables-persistent
 git clone -b stable/queens https://git.openstack.org/openstack-dev/devstack
 cp /tmp/local.conf devstack/
 cd devstack
