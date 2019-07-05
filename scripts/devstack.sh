28d791f698669b391c86fbedda2dfde369084ec7
Yuval Shalev <shalev67@gmail.com>
2019-07-05 13:17:23 +0300

    Remove sahara plugin and update ubuntu image

--- scripts/devstack.sh
+++ scripts/devstack.sh
@@ -2,7 +2,7 @@
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 
-sudo apt update && sudo apt install -y git uuid-runtime iptables iptables-persistent
+sudo apt update && sudo apt install -y git uuid-runtime iptables iptables-persistent bridge-utils
 git clone -b stable/queens https://git.openstack.org/openstack-dev/devstack
 cp /tmp/local.conf devstack/
 cd devstack
